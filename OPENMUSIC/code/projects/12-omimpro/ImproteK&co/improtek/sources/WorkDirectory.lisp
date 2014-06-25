bfd945d3e8947e97f7874f92a641ff5b53b8e4cf
jerome nika <jnika@m1804.ircam.fr>
2014-06-25 18:43:31 +0200

    yagayo

--- OPENMUSIC/code/projects/12-omimpro/ImproteK&co/improtek/sources/WorkDirectory.lisp
+++ OPENMUSIC/code/projects/12-omimpro/ImproteK&co/improtek/sources/WorkDirectory.lisp
@@ -1,4 +1,4 @@
 (IN-PACKAGE :OM)
-(SETF PATH_WORK_DIRECTORY NIL)
+(SETF PATH_WORK_DIRECTORY #P"/Users/jnika/Google Drive/Dev/Max-Interface/")
 
 ;To modify the path of the work directory, close this file and call (change_path_directory)
